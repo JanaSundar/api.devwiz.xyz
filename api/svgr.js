2ebe432071a33157ab72b52471499aba59bd5f95
Janarthanan <janasundar1997@gmail.com>
2023-07-16 08:56:36 +0530

    renamed response name

--- api/svgr.js
+++ api/svgr.js
@@ -12,8 +12,8 @@ module.exports = (req, res) => {
         ...req.body.options,
         plugins: [svgo, jsx, prettier],
     })
-        .then((output) => {
-            res.status(200).json({ output })
+        .then((result) => {
+            res.status(200).json({ result })
         })
         .catch((error) => {
             res.status(400).json({ error: error.message })
